3893ece7c74655d875d3116ffedd2cfdf5e88c8c
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-02-21 21:30:43 +0500

    added

--- app_flutter/README.md
+++ app_flutter/README.md
@@ -10,3 +10,11 @@
 - Wrap arguments inside curly brackets, then it is not necessary to include all arguments in the constructor
 - We can use like **(this.name, this.age)** stead of **(String name, int age)**
 - There is no need of assignation inside constructor when we are using above sytle.
+
+#### Type of Widgets
+- Visible 
+    - Buttons, Input fields
+- Invisible
+    - Coloumn, row etc
+- Container 
+    - in between visible and invisible
--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-    runApp(App());
+void main() {
+  runApp(App());
 }
 
-class App extends StatelessWidget
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return MaterialApp(home: Text('Hello world'));
-    }
+class App extends StatelessWidget {
+  final List<String> questions = [
+    'What\'s your fav color?',
+    'What\'s your fav animal?'
+  ];
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App 1'),
+        ),
+        body: Column(
+          children: [
+            Text('Question # 1'),
+            RaisedButton(child: Text('Answer'), onPressed: null),
+            RaisedButton(child: Text('Answer'), onPressed: null),
+            RaisedButton(child: Text('Answer'), onPressed: null),
+          ],
+        ),
+      ),
+    );
+  }
 }
